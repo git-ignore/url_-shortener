5e4238101b8cf01c3fd2d1b9d26905aba1ff3d30
S.Belov <s.belov@properm.ru>
2017-11-13 16:56:51 +0500

    + count of redirects report

--- api/v1/Resourses/Reports/Redirects.py
+++ api/v1/Resourses/Reports/Redirects.py
@@ -17,7 +17,6 @@ class RedirectsReport(Resource):
         args = self.reqparse.parse_args()
         from_date = datetime.strptime(args["from_date"], "%Y-%m-%d")
         to_date = datetime.strptime(args["to_date"], "%Y-%m-%d") + timedelta(days=1) - timedelta(minutes=1)
-
         raw_redirects = FollowUrl.select().where(
             (FollowUrl.datetime >= from_date) &
             (FollowUrl.datetime <= to_date) &
@@ -26,35 +25,48 @@ class RedirectsReport(Resource):
 
         redirects = []
         for raw_redirect in raw_redirects:
-            # redirect = model_to_dict(raw_redirect)
-            # redirect["datetime"] = redirect["datetime"].strftime('%d.%m.%Y %H:%M')
             redirects.append(model_to_dict(raw_redirect))
 
         dates = self.get_dates_array(from_date, to_date, group_by)
-        report = []
+        report = self.group_redirects(dates, redirects, group_by)
 
+        return report
+
+    def get_dates_array(self, from_date, to_date, group_by):
+        dates = [from_date]
+        added_date = from_date
+        while added_date < to_date:
+            added_date = self.add_timedelta(added_date, group_by)
+            dates.append(added_date)
+        return dates
+
+    def group_redirects(self, dates, redirects, group_by):
+
+        # set report points according to group_by attribute
+        report = dict.fromkeys(map(self.time_to_string, dates), 0)
+
+        # counting redirects and group for each point in report
         for redirect in redirects:
             for i, date in enumerate(dates):
-                if i != 1 and i != len(dates)-1:
-                    if (redirect["datetime"] <= dates[i-1]) and (redirect["datetime"] >= dates[i+1]):
-                        report.append({date.strftime('%d.%m.%Y %H:%M'): 1})
+                if i < len(dates) - 1 and date < redirect["datetime"] < (self.add_timedelta(date, group_by)):
+                    str_date = self.time_to_string(date)
+                    report[str_date] += 1
 
         return report
 
     @staticmethod
-    def get_dates_array(from_date, to_date, group_by):
-        #dates = [from_date.strftime('%d.%m.%Y %H:%M')]
-        dates = [from_date]
-        added_date = from_date
-        while added_date < to_date:
-            if group_by == "days":
-                added_date += timedelta(days=1)
-            elif group_by == "hours":
-                added_date += timedelta(hours=1)
-            elif group_by == "minutes":
-                added_date += timedelta(minutes=1)
-            else:
-                return False
-            dates.append(added_date)
-        return dates
+    def add_timedelta(date, group_by):
+        if group_by == "days":
+            date += timedelta(days=1)
+        elif group_by == "hours":
+            date += timedelta(hours=1)
+        elif group_by == "minutes":
+            date += timedelta(minutes=1)
+        else:
+            return False
+        return date
+
+    @staticmethod
+    def time_to_string(time):
+        return time.strftime('%d.%m.%Y %H:%M')
 
